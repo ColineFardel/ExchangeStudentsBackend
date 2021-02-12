7bfdfb18b657d6ae032792470fb8768c182432f5
ColineFardel <coline.fardel@gmail.com>
2021-02-12 12:06:40 +0200

    First commit

---
+++ src/main/java/com/example/ExchangeStudentsBackend/ExchangeStudentsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExchangeStudentsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeStudentsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeStudentsBackendApplication.class, args);
+	}
+
+}
