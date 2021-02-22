4eaef63748701f1889dd7ca7667670eca7d14084
ColineFardel <coline.fardel@gmail.com>
2021-02-22 18:58:38 +0200

    Trying to fix tests

--- src/test/java/com/example/ExchangeStudentsBackend/ExchangeStudentsBackendApplicationTests.java
+++ src/test/java/com/example/ExchangeStudentsBackend/ExchangeStudentsBackendApplicationTests.java
@@ -1,14 +1,16 @@
 package com.example.ExchangeStudentsBackend;
 import static org.assertj.core.api.Assertions.assertThat;
+
+//import org.junit.Test;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.ExchangeStudentsBackend.web.ExchangeStudentsController;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 class ExchangeStudentsBackendApplicationTests {
 	
