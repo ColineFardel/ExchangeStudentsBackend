fee64162fb408ff0e0420f7e1a7dbf885fbcf211
ColineFardel <coline.fardel@gmail.com>
2021-05-02 14:33:45 +0300

    Change get user to post request

--- src/main/java/com/example/ExchangeStudentsBackend/web/ExchangeStudentsController.java
+++ src/main/java/com/example/ExchangeStudentsBackend/web/ExchangeStudentsController.java
@@ -115,8 +115,8 @@ public class ExchangeStudentsController {
 	}
 
 	// Get one user
-	@RequestMapping(value = "/user/{username}", method = RequestMethod.GET)
-	public @ResponseBody User getUser(@PathVariable("username") String username) {
+	@PostMapping(value = "/user")
+	public User getUser(@RequestBody String username) {
 		return userrepo.findByUsername(username);
 	}
 
